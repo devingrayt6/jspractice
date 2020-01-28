7ee637c10d4dd3952b2c38c5a9fe7c1c75fc688d
devingrayt6 <devingrayt6@gmail.com>
2020-01-28 15:47:36 -0700

    camelCase bonus challenge

--- app/bonuses.js
+++ app/bonuses.js
@@ -2,5 +2,17 @@
 let str = "team number 2"
 
 function camelCase(str) {
+  let newArr = [];
+  let toArr = str.split(" ");
 
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == ' ' && i != 0) {
+      newArr.push(str[i + 1].toUpperCase())
+    }
+    else if (str[i - 1] != ' ') {
+      newArr.push(str[i])
+    }
+  }
+
+  return newArr.join('');
 }
